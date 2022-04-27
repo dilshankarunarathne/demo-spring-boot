5506813c417453a49e8e4db24355a104d495b5ea
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 19:04:23 +0530

    Update FakeDataDaoTest.java

--- src/test/java/me/karunarathne/learningspringboot/dao/FakeDataDaoTest.java
+++ src/test/java/me/karunarathne/learningspringboot/dao/FakeDataDaoTest.java
@@ -64,6 +64,7 @@ class FakeDataDaoTest {
         Optional <User> user = fakeDataDao.selectUserByUserUid(joeUserUid) ;
         assertThat(user.isPresent()).isTrue() ;
         assertThat(fakeDataDao.selectAllUsers()).hasSize(1) ;
+        assertThat(user.get()).isEqualToComparingFieldByField(newJoe) ;
     }
 
     @Test
