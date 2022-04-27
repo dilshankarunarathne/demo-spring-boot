9cd3c032459a699af09325495884bd099cb61cf6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 16:32:52 +0530

    Update UserService.java

--- src/main/java/me/karunarathne/learningspringboot/service/UserService.java
+++ src/main/java/me/karunarathne/learningspringboot/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -21,7 +22,7 @@ public class UserService {
         return userDao.selectAllUsers() ;
     }
 
-    public User getUser(UUID userUid) {
+    public Optional <User> getUser(UUID userUid) {
         return null ;
     }
 
