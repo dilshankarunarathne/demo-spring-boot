1a57435edbae12c71b0104bc003e3219ecdbd49a
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 16:41:04 +0530

    Create FakeDataDaoTest.java

---
+++ src/test/java/me/karunarathne/learningspringboot/dao/FakeDataDaoTest.java
@@ -0,0 +1,33 @@
+package me.karunarathne.learningspringboot.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FakeDataDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void selectAllUsers() {
+    }
+
+    @Test
+    void selectUserByUserUid() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUserByUserUid() {
+    }
+
+    @Test
+    void insertUser() {
+    }
+}
