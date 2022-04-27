768717a0bd7506ccf0ce389c91e44e7617d9a4cd
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 18:52:45 +0530

    Update FakeDataDaoTest.java

--- src/test/java/me/karunarathne/learningspringboot/dao/FakeDataDaoTest.java
+++ src/test/java/me/karunarathne/learningspringboot/dao/FakeDataDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
@@ -42,6 +43,7 @@ class FakeDataDaoTest {
 
         assertThat(fakeDataDao.selectAllUsers()).hasSize(2) ;
 
+        Optional <User> user = fakeDataDao.selectUserByUserUid(annaUserUid) ;
         
     }
 
