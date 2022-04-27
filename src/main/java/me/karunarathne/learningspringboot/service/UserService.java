6786588f7bc8ec8b970d7faa75e78e5d912c69bf
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 16:25:56 +0530

    Update UserService.java

--- src/main/java/me/karunarathne/learningspringboot/service/UserService.java
+++ src/main/java/me/karunarathne/learningspringboot/service/UserService.java
@@ -11,6 +11,7 @@ public class UserService {
     private UserDao userDao ;
 
     public UserService (UserDao userDao) {
+        this.userDao = userDao ;
     }
 
     public List<User> getAllUsers() {
