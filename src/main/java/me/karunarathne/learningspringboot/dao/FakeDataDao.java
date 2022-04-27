6686da91e60f9648711e3ed0b13a6426e686c259
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 15:58:04 +0530

    Update FakeDataDao.java

--- src/main/java/me/karunarathne/learningspringboot/dao/FakeDataDao.java
+++ src/main/java/me/karunarathne/learningspringboot/dao/FakeDataDao.java
@@ -1,5 +1,33 @@
 package me.karunarathne.learningspringboot.dao;
 
+import me.karunarathne.learningspringboot.model.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public class FakeDataDao implements UserDao {
-    
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUser(UUID userUd) {
+        return null;
+    }
+
+    @Override
+    public int updateUser(User user) {
+        return 0;
+    }
+
+    @Override
+    public int removeUser(UUID userUid) {
+        return 0;
+    }
+
+    @Override
+    public int insertUser(User user) {
+        return 0;
+    }
 }
