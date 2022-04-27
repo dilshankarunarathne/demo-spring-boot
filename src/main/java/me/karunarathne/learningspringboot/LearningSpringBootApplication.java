b42c691f70e6b225a76bb4561922021c09ad0dcf
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 10:06:53 +0530

    Update LearningSpringBootApplication.java

--- src/main/java/me/karunarathne/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/me/karunarathne/learningspringboot/LearningSpringBootApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearningSpringBootApplication.class, args);
+		SpringApplication.run (
+				LearningSpringBootApplication.class, args
+		);
 	}
-
 }
