fc7fafff31dbbcdc064ad6fd036d3a2eb83098af
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 18:56:51 +0530

    Update FakeDataDao.java

--- src/main/java/me/karunarathne/learningspringboot/dao/FakeDataDao.java
+++ src/main/java/me/karunarathne/learningspringboot/dao/FakeDataDao.java
@@ -26,7 +26,7 @@ public class FakeDataDao implements UserDao {
 
     @Override
     public Optional <User> selectUserByUserUid(UUID userUid) {
-        return Optional.ofNullable(database.get(UUID.randomUUID()));
+        return Optional.ofNullable(database.get(userUid));
     }
 
     @Override
