9c0c4a8e00bd325c142d18d71e6437a582dcf553
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 16:09:42 +0530

    Update UserService.java

--- src/main/java/me/karunarathne/learningspringboot/service/UserService.java
+++ src/main/java/me/karunarathne/learningspringboot/service/UserService.java
@@ -1,5 +1,37 @@
 package me.karunarathne.learningspringboot.service;
 
-public class UserService {
+import me.karunarathne.learningspringboot.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class UserService {
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(database.values());
+    }
+
+    @Override
+    public User getUser(UUID userUid) {
+        return database.get(userUid);
+    }
+
+    @Override
+    public int updateUser(User user) {
+        database.put(user.getUserUid(), user) ;
+        return 1;
+    }
+
+    @Override
+    public int removeUser(UUID userUid) {
+        database.remove(userUid) ;
+        return 1;
+    }
+
+    @Override
+    public int insertUser(UUID userUid, User user) {
+        database.put(userUid, user) ;
+        return 1;
+    }
 }
