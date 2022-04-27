7bafd6512115223714f978711989425cc9a58af9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 18:43:54 +0530

    Update FakeDataDaoTest.java

--- src/test/java/me/karunarathne/learningspringboot/dao/FakeDataDaoTest.java
+++ src/test/java/me/karunarathne/learningspringboot/dao/FakeDataDaoTest.java
@@ -20,6 +20,7 @@ class FakeDataDaoTest {
     void shouldSelectAllUsers() throws Exception {
         List<User> users = fakeDataDao.selectAllUsers();
         assertThat(users).hasSize(1) ;
+        User user = users.get(0) ;
         
     }
 
