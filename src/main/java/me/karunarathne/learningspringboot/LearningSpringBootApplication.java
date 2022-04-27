fd7113c2cce9cd803fa0375969390d57ed528767
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 15:52:03 +0530

    Working on

--- src/main/java/me/karunarathne/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/me/karunarathne/learningspringboot/LearningSpringBootApplication.java
@@ -2,6 +2,9 @@ package me.karunarathne.learningspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringBootApplication {
---
+++ src/main/java/me/karunarathne/learningspringboot/model/User.java
@@ -0,0 +1,61 @@
+package me.karunarathne.learningspringboot.model;
+
+import java.util.UUID;
+
+public class User {
+    private final UUID userUid ;
+    private final String firstName ;
+    private final String lastName ;
+    private final Gender gender ;
+    private final Integer age ;
+    private final String email ;
+
+    enum Gender {
+        MALE, FEMALE
+    }
+
+    public User(UUID userUid, String firstName, String lastName, Gender gender, Integer age, String email) {
+        this.userUid = userUid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userUid=" + userUid +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public UUID getUserUid() {
+        return userUid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
