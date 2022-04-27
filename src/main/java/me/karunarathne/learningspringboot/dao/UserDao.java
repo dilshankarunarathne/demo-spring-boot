2f58ce614a729112a6265e16114910d858fc47c7
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 15:55:59 +0530

    Update UserDao.java

--- src/main/java/me/karunarathne/learningspringboot/dao/UserDao.java
+++ src/main/java/me/karunarathne/learningspringboot/dao/UserDao.java
@@ -1,5 +1,14 @@
 package me.karunarathne.learningspringboot.dao;
 
+import me.karunarathne.learningspringboot.model.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserDao {
-    
+    List<User> getAllUsers () ;
+    User getUser (UUID userUd) ;
+    int updateUser (User user) ;
+    int removeUser (UUID userUid) ;
+    int insertUser (User user) ;
 }
