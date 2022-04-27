404ed29d055ba49024fdc782945758b5dd9152fb
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-27 21:14:11 +0530

    new tests

---
+++ src/test/java/me/karunarathne/learningspringboot/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package me.karunarathne.learningspringboot.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void removeUser() {
+    }
+
+    @Test
+    void insertUser() {
+    }
+}
