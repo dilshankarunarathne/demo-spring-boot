ad11e572c362826d9592396e5109c3f056b31b90
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 15:37:35 +0530

    Update ResteasyConfig.java

--- src/main/java/me/karunarathne/learningspringboot/config/ResteasyConfig.java
+++ src/main/java/me/karunarathne/learningspringboot/config/ResteasyConfig.java
@@ -1,6 +1,9 @@
 package me.karunarathne.learningspringboot.config;
 
+import org.springframework.context.annotation.Configuration;
+
 import javax.ws.rs.core.Application;
 
+@Configuration
 public class ResteasyConfig extends Application {
 }
