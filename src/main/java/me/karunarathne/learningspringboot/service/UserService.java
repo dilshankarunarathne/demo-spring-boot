b0ba290b107a73fd6edb39caba1c5b1363e1d541
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 12:10:53 +0530

    Update UserService.java

--- src/main/java/me/karunarathne/learningspringboot/service/UserService.java
+++ src/main/java/me/karunarathne/learningspringboot/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -25,8 +26,9 @@ public class UserService {
         }
         try {
             User.Gender theGender = User.Gender.valueOf(gender.get()) ;
-            return users.stream () 
-                    .filter ()
+            return users.stream ()
+                    .filter ( user -> user.getGender().equals(theGender) )
+                    .collect(Collectors.toList()) ;
         } catch (Exception e) {
             throw new IllegalStateException ("Invalid gender", e) ;
         }
