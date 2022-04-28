5b36b6d2c9b2b79570e483e3e0ccfaa1b1304cba
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 09:36:29 +0530

    Update UserResource.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
@@ -32,7 +32,7 @@ public class UserResource {
 
     @RequestMapping(
             method = RequestMethod.GET,
-            path = ""
+            path = "{}"
     )
     public User fetchUser () {
         return userService.getUser () ;
