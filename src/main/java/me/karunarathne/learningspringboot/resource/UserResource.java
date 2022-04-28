923024dca7d0ca8e716a74c6099ae48e93d2053a
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 09:24:37 +0530

    Update UserResource.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
@@ -2,10 +2,13 @@ package me.karunarathne.learningspringboot.resource;
 
 import me.karunarathne.learningspringboot.model.User;
 import me.karunarathne.learningspringboot.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class UserResource {
     private UserService userService ;
 
