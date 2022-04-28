0b8d36a553c2016bf8452b32bc97abbd0dd15d8a
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 15:42:49 +0530

    Update UserResourceResteasy.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResourceResteasy.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResourceResteasy.java
@@ -3,9 +3,11 @@ package me.karunarathne.learningspringboot.resource;
 import me.karunarathne.learningspringboot.model.User;
 import me.karunarathne.learningspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +22,7 @@ public class UserResourceResteasy {
     }
 
     @GET
-    
+    @Produces (MediaType.APPLICATION_JSON)
     public List<User> fetchUsers (@QueryParam("gender") String gender) {
         return userService.getAllUsers(Optional.ofNullable(gender)) ;
     }
