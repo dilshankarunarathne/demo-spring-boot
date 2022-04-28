d1c1eb7843149d9b57857385aa214cadfc456f07
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 09:43:20 +0530

    Update UserResource.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
@@ -4,6 +4,7 @@ import me.karunarathne.learningspringboot.model.User;
 import me.karunarathne.learningspringboot.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,6 @@ public class UserResource {
         if (userOptional.isPresent()) {
             return ResponseEntity.ok(userOptional.get()) ;
         }
-        
+        return ResponseEntity.status(HttpStatus.NOT_FOUND) ;
     }
 }
