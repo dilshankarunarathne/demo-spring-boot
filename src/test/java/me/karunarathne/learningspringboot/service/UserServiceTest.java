27c3e3a606609500c2db201adc7365b8ab52e5d3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 12:23:30 +0530

    Update UserServiceTest.java

--- src/test/java/me/karunarathne/learningspringboot/service/UserServiceTest.java
+++ src/test/java/me/karunarathne/learningspringboot/service/UserServiceTest.java
@@ -73,6 +73,7 @@ class UserServiceTest {
         given (fakeDataDao.selectAllUsers()).willReturn(users) ;
 
         List <User> filteredUsers = userService.getAllUsers(Optional.of("MALE")) ;
+        
     }
 
     @Test
