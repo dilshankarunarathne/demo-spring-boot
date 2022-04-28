595d4bc025e8d42f149425a8867136cd02fa50c5
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 09:30:48 +0530

    Update UserResource.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping (
+        path = "/api/v1/users"
+)
 public class UserResource {
     private UserService userService ;
 
