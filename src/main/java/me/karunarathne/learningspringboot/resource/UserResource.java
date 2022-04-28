dbed5d09e787c0dca7b62313f0e52753224be1ba
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 09:46:19 +0530

    Update UserResource.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResource.java
@@ -56,6 +56,12 @@ public class UserResource {
             this.message = message;
         }
 
-        
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
