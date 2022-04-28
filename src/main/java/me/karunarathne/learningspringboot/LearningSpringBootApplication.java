f2aff88085ff91b6a0aadfde2d849c6b7dc8e3f4
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 09:08:18 +0530

    Update LearningSpringBootApplication.java

--- src/main/java/me/karunarathne/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/me/karunarathne/learningspringboot/LearningSpringBootApplication.java
@@ -2,9 +2,6 @@ package me.karunarathne.learningspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringBootApplication {
