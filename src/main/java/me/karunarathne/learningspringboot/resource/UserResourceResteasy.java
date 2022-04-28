85e33d5674a6153ebac95339cc0942794219c917
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-28 15:46:18 +0530

    Update UserResourceResteasy.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResourceResteasy.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResourceResteasy.java
@@ -3,12 +3,12 @@ package me.karunarathne.learningspringboot.resource;
 import me.karunarathne.learningspringboot.model.User;
 import me.karunarathne.learningspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.Optional;
 
