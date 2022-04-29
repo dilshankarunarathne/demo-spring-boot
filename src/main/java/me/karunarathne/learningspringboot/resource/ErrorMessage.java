82499c5f70009ab05078febed7407a7fdb672e7f
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-29 09:31:39 +0530

    Update ErrorMessage.java

--- src/main/java/me/karunarathne/learningspringboot/resource/ErrorMessage.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/ErrorMessage.java
@@ -1,4 +1,14 @@
 package me.karunarathne.learningspringboot.resource;
 
 public class ErrorMessage {
+    String errorMessage ;
+
+    public ErrorMessage(String message) {
+        this.errorMessage = message;
+    }
+
+    public String getMessage() {
+        return errorMessage;
+    }
+
 }
