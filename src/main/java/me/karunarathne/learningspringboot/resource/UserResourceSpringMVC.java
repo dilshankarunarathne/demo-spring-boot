dfcfe97b8a51575e24f74e00107033bea26e999d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-29 09:34:42 +0530

    Update UserResourceSpringMVC.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResourceSpringMVC.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResourceSpringMVC.java
@@ -4,15 +4,21 @@ import me.karunarathne.learningspringboot.model.User;
 import me.karunarathne.learningspringboot.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@RestController
+@RequestMapping (
+        path = "/api/v1/users"
+)
 public class UserResourceSpringMVC {
     private UserService userService ;
 
@@ -21,53 +27,80 @@ public class UserResourceSpringMVC {
         this.userService = userService ;
     }
 
-    @GET
-    @Produces (MediaType.APPLICATION_JSON)
+    @RequestMapping (
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public List<User> fetchUsers ( @QueryParam("gender") String gender) {
         return userService.getAllUsers(Optional.ofNullable(gender)) ;
     }
 
-    @GET
-    @Produces (MediaType.APPLICATION_JSON)
-    @Path ("{userUid}")
-    public Response fetchUser (@PathParam("userUid") UUID userUid) {
-        Optional <User> userOptional = userService.getUser (userUid) ;
-        if (userOptional.isPresent()) {
-            return Response.ok(userOptional.get()).build() ;
-        }
-        return Response.status (Response.Status.NOT_FOUND)
-                .entity (new ErrorMessage ("user " + userUid + " was not found !"))
-                .build() ;
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "{userUid}"
+    )
+    public ResponseEntity <?> fetchUser (@PathVariable ("userUid") UUID userUid) {
+        return userService.getUser(userUid)
+                .<ResponseEntity <?>> map(ResponseEntity::ok)
+                .orElseGet (
+                        () -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                .body (
+                                        new ErrorMessage ("user " + userUid + " was not found !")
+                                )
+                ) ;
     }
 
-    @POST
-    @Consumes (MediaType.APPLICATION_JSON)
-    @Produces (MediaType.APPLICATION_JSON)
-    public Response insertNewUser (User user) {
+    @RequestMapping (
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity <Integer> insertNewUser (@RequestBody User user) {
         int result = userService.insertUser (user) ;
         return getIntegerResponseEntity (result) ;
     }
 
-    @PUT
-    @Consumes (MediaType.APPLICATION_JSON)
-    @Produces (MediaType.APPLICATION_JSON)
-    public Response updateUser (User user) {
+    @RequestMapping (
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity <Integer> updateUser (@RequestBody User user) {
         int result = userService.updateUser (user) ;
         return getIntegerResponseEntity (result) ;
     }
 
-    @DELETE
-    @Produces (MediaType.APPLICATION_JSON)
-    @Path ("{userUid}")
-    public Response deleteUser (@PathParam ("userUid") UUID userUid) {
+    @RequestMapping (
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "{userUid}"
+    )
+    public ResponseEntity <Integer> deleteUser (@PathVariable ("userUid") UUID userUid) {
         int result = userService.removeUser (userUid) ;
         return getIntegerResponseEntity (result) ;
     }
 
-    private Response getIntegerResponseEntity (int result) {
+    private ResponseEntity<Integer> getIntegerResponseEntity (int result) {
         if (result == 1) {
-            return Response.ok().build() ;
+            return ResponseEntity.ok().build();
         }
-        return Response.status(result).build() ;
+        return ResponseEntity.badRequest().build();
+    }
+
+    class ErrorMessage {
+        String errorMessage ;
+
+        public ErrorMessage(String message) {
+            this.errorMessage = message;
+        }
+
+        public String getMessage() {
+            return errorMessage;
+        }
+
+        public void setMessage(String message) {
+            this.errorMessage = message;
+        }
     }
 }
