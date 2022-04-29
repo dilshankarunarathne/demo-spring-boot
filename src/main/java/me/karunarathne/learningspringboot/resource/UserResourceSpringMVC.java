7006e0dcd87c18d9bc5a83fcc8565d95f34f1952
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-29 09:30:00 +0530

    Update UserResourceSpringMVC.java

--- src/main/java/me/karunarathne/learningspringboot/resource/UserResourceSpringMVC.java
+++ src/main/java/me/karunarathne/learningspringboot/resource/UserResourceSpringMVC.java
@@ -5,9 +5,6 @@ import me.karunarathne.learningspringboot.service.UserService;
 
 import org.checkerframework.checker.nullness.Opt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
