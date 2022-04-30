ef1558aa9a30b4ccd561c0e612030a5f9330c030
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-30 09:28:23 +0530

    Update UserResourceV1.java

--- src/main/java/me/karunarathne/learningspringboot/clientproxy/UserResourceV1.java
+++ src/main/java/me/karunarathne/learningspringboot/clientproxy/UserResourceV1.java
@@ -5,6 +5,7 @@ import me.karunarathne.learningspringboot.model.User;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import java.util.List;
 import java.util.UUID;
 
