fb55015f151d41c054fc3a35c3adbfb44b083ba3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-30 09:26:02 +0530

    Update UserResourceV1.java

--- src/main/java/me/karunarathne/learningspringboot/clientproxy/UserResourceV1.java
+++ src/main/java/me/karunarathne/learningspringboot/clientproxy/UserResourceV1.java
@@ -1,5 +1,58 @@
 package me.karunarathne.learningspringboot.clientproxy;
 
-public interface UserResourceV1 { 
+import me.karunarathne.learningspringboot.model.User;
+import me.karunarathne.learningspringboot.resource.ErrorMessage;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+public interface UserResourceV1 {
+    @GET
+    @Produces(APPLICATION_JSON)
+    public List<User> fetchUsers (@QueryParam("gender") String gender) {
+        return userService.getAllUsers(Optional.ofNullable(gender)) ;
+    }
+
+    @GET
+    @Produces (MediaType.APPLICATION_JSON)
+    @Path("{userUid}")
+    public Response fetchUser (@PathParam("userUid") UUID userUid) {
+        Optional <User> userOptional = userService.getUser (userUid) ;
+        if (userOptional.isPresent()) {
+            return Response.ok(userOptional.get()).build() ;
+        }
+        return Response.status (Response.Status.NOT_FOUND)
+                .entity (new ErrorMessage("user " + userUid + " was not found !"))
+                .build() ;
+    }
+
+    @POST
+    @Consumes (MediaType.APPLICATION_JSON)
+    @Produces (MediaType.APPLICATION_JSON)
+    public Response insertNewUser (User user) {
+        int result = userService.insertUser (user) ;
+        return getIntegerResponseEntity (result) ;
+    }
+
+    @PUT
+    @Consumes (MediaType.APPLICATION_JSON)
+    @Produces (MediaType.APPLICATION_JSON)
+    public Response updateUser (User user) {
+        int result = userService.updateUser (user) ;
+        return getIntegerResponseEntity (result) ;
+    }
+
+    @DELETE
+    @Produces (MediaType.APPLICATION_JSON)
+    @Path ("{userUid}")
+    public Response deleteUser (@PathParam ("userUid") UUID userUid) {
+        int result = userService.removeUser (userUid) ;
+        return getIntegerResponseEntity (result) ;
+    }
 }
