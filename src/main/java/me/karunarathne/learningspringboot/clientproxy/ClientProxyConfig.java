35b16a6518c30c7cfb02ae7e98c7e2480d6554b9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-30 09:40:11 +0530

    Update ClientProxyConfig.java

--- src/main/java/me/karunarathne/learningspringboot/clientproxy/ClientProxyConfig.java
+++ src/main/java/me/karunarathne/learningspringboot/clientproxy/ClientProxyConfig.java
@@ -3,7 +3,6 @@ package me.karunarathne.learningspringboot.clientproxy;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
