8f414aa5cfb251f97508c263a1f8b458645ec27c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-30 09:44:40 +0530

    Update ClientProxyConfig.java

--- src/main/java/me/karunarathne/learningspringboot/clientproxy/ClientProxyConfig.java
+++ src/main/java/me/karunarathne/learningspringboot/clientproxy/ClientProxyConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ClientProxyConfig {
 
-    @Value ()
+    @Value ("${users.api.url.v1}")
     private int usersEndPointUrl ;
 
     @Bean
