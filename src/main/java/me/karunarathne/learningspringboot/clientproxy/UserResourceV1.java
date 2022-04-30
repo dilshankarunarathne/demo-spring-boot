4cc5dd94754fd839ec9649c2b95f65c4cdace683
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-30 09:27:48 +0530

    Update UserResourceV1.java

--- src/main/java/me/karunarathne/learningspringboot/clientproxy/UserResourceV1.java
+++ src/main/java/me/karunarathne/learningspringboot/clientproxy/UserResourceV1.java
@@ -1,5 +1,17 @@
 package me.karunarathne.learningspringboot.clientproxy;
 
+import me.karunarathne.learningspringboot.model.User;
+import me.karunarathne.learningspringboot.resource.ErrorMessage;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 public interface UserResourceV1 {
     @GET
     @Produces(APPLICATION_JSON)
