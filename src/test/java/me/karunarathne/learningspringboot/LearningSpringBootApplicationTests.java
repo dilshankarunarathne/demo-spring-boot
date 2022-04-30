9b998c8f0a40a48efcc8391baa33df911adc2aa8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-30 10:49:24 +0530

    Update LearningSpringBootApplicationTests.java

--- src/test/java/me/karunarathne/learningspringboot/LearningSpringBootApplicationTests.java
+++ src/test/java/me/karunarathne/learningspringboot/LearningSpringBootApplicationTests.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith ()
+@RunWith (SpringRunner.class)
 @SpringBootTest
 class LearningSpringBootApplicationTests {
 
