fe675a8089ef0acbfc7f7a25333f197977e5fd4c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-04-30 09:44:21 +0530

    configs

--- src/main/java/me/karunarathne/learningspringboot/clientproxy/ClientProxyConfig.java
+++ src/main/java/me/karunarathne/learningspringboot/clientproxy/ClientProxyConfig.java
@@ -3,11 +3,15 @@ package me.karunarathne.learningspringboot.clientproxy;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClientProxyConfig {
+
+    @Value()
+    private int usersEndPointUrl ;
 
     @Bean
     public UserResourceV1 getUserResourceV1 () {
